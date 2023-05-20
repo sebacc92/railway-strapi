cadd956771fe30f9820a8f2fa1af8fc9c6e4354f
Seba Cardoso <95690554+sebacc92@users.noreply.github.com>
2023-05-20 00:53:24 -0300

    Eliminado lista de emprendimientos

--- src/api/emrendimiento/controllers/emrendimiento.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * emrendimiento controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::emrendimiento.emrendimiento');
--- src/api/emrendimiento/routes/emrendimiento.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * emrendimiento router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::emrendimiento.emrendimiento');
