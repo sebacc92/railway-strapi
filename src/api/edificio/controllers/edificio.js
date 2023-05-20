c28e84f3718c75edea6909325fc4da191ef4a534
Seba Cardoso <95690554+sebacc92@users.noreply.github.com>
2023-05-20 00:45:02 -0300

    agregados edificios

---
+++ src/api/edificio/controllers/edificio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * edificio controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::edificio.edificio');
---
+++ src/api/edificio/routes/edificio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * edificio router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::edificio.edificio');
